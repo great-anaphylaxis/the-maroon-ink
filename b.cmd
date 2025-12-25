07d7899d6b67922d3f914fdaebbf24fbb378c54c
Jhareign <74128037+Jhareign@users.noreply.github.com>
2025-12-25 23:51:59 +0800

    First commit

---
+++ b.cmd
@@ -0,0 +1,8 @@
+git add .
+git commit -m %1
+git remote add origin https://github.com/great-anaphylaxis/the-maroon-ink.git
+git remote -v
+git push -f origin master
+
+REM Github actions already exists
+REM firebase deploy --only hosting:jhareign

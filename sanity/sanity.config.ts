223ff1ce35db35c9483994473c435bf3192b43c1
Jhareign <74128037+Jhareign@users.noreply.github.com>
2025-12-26 00:33:42 +0800

    sanity

---
+++ sanity/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'The Maroon Ink',
+
+  projectId: 'w7ogeebt',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})

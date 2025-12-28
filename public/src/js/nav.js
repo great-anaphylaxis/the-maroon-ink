a6200f8a3fcef3b2836d531c7a2e61738a8902c6
Jhareign <74128037+Jhareign@users.noreply.github.com>
2025-12-28 13:08:43 +0800

    Nav movement

--- public/src/js/nav.js
+++ public/src/js/nav.js
@@ -1,7 +1,13 @@
-const nav = document.querySelector('nav');
 
-let pastScrollPos = 0
-let scrollPos = 0
+
+const navtop = document.getElementById('navtop');
+const navside = document.getElementById('navside');
+const content = document.getElementById('content');
+const optionsButton = document.getElementById('options-icon');
+
+let pastScrollPos = 0;
+let scrollPos = 0;
+let hidden = false;
 
 function onscroll() {
     if (window.innerWidth >= 950) {
@@ -19,14 +25,29 @@ function onscroll() {
         }
     
         if (scrollPos > pastScrollPos) {
-            nav.style.animation = "0.4s ease 0s 1 normal forwards running navbar-hide";
+            navtop.style.animation = "0.4s ease 0s 1 normal forwards running navbar-hide";
         }
     
         else {
-            nav.style.animation = "0.4s ease 0s 1 normal forwards running navbar-show";
+            navtop.style.animation = "0.4s ease 0s 1 normal forwards running navbar-show";
         }
     });
 }
 
+function optionsButtonClick() {
+    hidden = !hidden;
+
+    if (hidden) {
+        navside.style.animation = "0.4s ease 0s 1 normal forwards running navside-hide";
+        content.style.animation = "0.4s ease 0s 1 normal forwards running navside-hide2";
+    }
+
+    else {
+        navside.style.animation = "0.4s ease 0s 1 normal forwards running navside-show";
+        content.style.animation = "0.4s ease 0s 1 normal forwards running navside-show2";
+    }
+}
+
 
 window.onscroll = onscroll;
+optionsButton.addEventListener('click', optionsButtonClick)

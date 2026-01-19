1dfb6132182a4daab22bf0b5d08a6ac4496cfa7e
Great Anaphylaxis <74128037+great-anaphylaxis@users.noreply.github.com>
2026-01-19 16:30:50 +0800

    Update nav.js

--- public/src/js/utils/nav.js
+++ public/src/js/utils/nav.js
@@ -1,4 +1,4 @@
-import { setTextAnimation } from "../lib/svg-text-animation.js";
+import { setTextAnimation, setReverseTextAnimation } from "../lib/svg-text-animation.js";
 
 export const navtop = document.getElementById('navtop');
 const navside = document.getElementById('navside');
@@ -238,7 +238,7 @@ export function showLoadingScreen(instant = false, fadeout = false) {
     }
 
     else if (fadeout) {
-        setTextAnimation(0.03, 1.4, 2, 'cubic-bezier( 0.50, 0.01, 0.00, 1.04 )', '#800000', false, "reverse")
+        setReverseTextAnimation(0.03, 1.4, 2, 'cubic-bezier( 0.50, 0.01, 0.00, 1.04 )', '#800000', false)
     }
     
     let t;
